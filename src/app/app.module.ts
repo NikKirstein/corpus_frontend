98c98fba5369be0ccf2c5acd074466cd93181686
markfullmer <mfullmer@gmail.com>
2017-08-20 17:46:06 -0500

    Complete basic search logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpModule }    from '@angular/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
 import { TextComponent } from './text.component';
 import { RestAPI } from './restAPI.service';
@@ -41,6 +42,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpModule,
     RouterModule.forRoot(
       appRoutes,
--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { RestAPI } from './restAPI.service';
 import { ActivatedRoute, Params }   from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './templates/search.component.html',
@@ -12,6 +15,7 @@ export class SearchComponent {
   constructor(
     private RestAPI: RestAPI,
     private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -24,13 +28,31 @@ export class SearchComponent {
   }
 
   SearchResults: {};
+  NotFound: boolean;
 
   get(): void {
     var id = this.route.params['_value'].string;
     this.RestAPI.getSearchResults(id).then(response => {
-      this.SearchResults = response;
-      console.log(this.SearchResults);
+      if (response.search_results != 'undefined') {
+        this.SearchResults = response;
+      }
     });
+  }
+
+  search(term: string): void {
+    this.NotFound = false;
+    if (term == '') {
+    }
+    else {
+      this.RestAPI.getSearchResults(term).then(response => {
+        this.SearchResults = response;
+        console.log(response.search_results);
+        if (typeof response.search_results === 'undefined') {
+          this.NotFound = true;
+        }
+        this.router.navigate(["search", term]);
+      });
+    }
   }
 
 }
